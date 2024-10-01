745e1c2a0a950688596edad9f00f3ee2193b6a5c
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-01 16:57:48 -0500

    feat: add base project

---
+++ src/main/java/com/emazon/microservicio_reporte/MicroservicioReporteApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.microservicio_reporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioReporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioReporteApplication.class, args);
+	}
+
+}
